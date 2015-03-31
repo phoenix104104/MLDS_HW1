1840fe34c3aea7d35c8eb951ec7d8a0cbb1d8aa6
phoenix104104 <phoenix104104@gmail.com>
2015-03-31 19:51:26 +0800

    train output acc_all

--- dnn.py
+++ dnn.py
@@ -76,8 +76,8 @@ class DNN:
 
 
     def train(self, X_train, Y_train, X_valid, Y_valid):
+        acc_all = []
         for i in range(self.epoch):
-            # TODO: batch
             starts = range(0, len(X_train), self.batch_size)
             ends = range(self.batch_size, len(X_train), self.batch_size) + [len(X_train)]
             randperm = np.random.permutation(len(X_train))
@@ -89,6 +89,8 @@ class DNN:
                     Y_round.append(Y_train[rpidx])
                 self.train_epoch(X_round,Y_round)
             acc = np.mean(np.argmax(Y_valid, axis=1) == self.predict(X_valid) )
+            acc_all.append(acc)
             print "Epoch %d, accuracy = %f" %(i, acc) 
-
+        
+        return acc_all
 
